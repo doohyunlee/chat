d0162528675109dadbca9948c72f204f713ff704
bbongdoo <bbongdoo@qdillion.com>
2017-06-21 15:33:08 +0900

    2차커밋

--- webserver.js
+++ webserver.js
@@ -1,11 +1,25 @@
-var http = require('http');
+var express = require('express');
 var fs = require('fs');
+var app = express();
 
-http.createServer(function(req, res){
+app.get('/imgs', function(req, res){
+    fs.readFile('me.jpg',function (err, data){
+        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        res.end(data);
+    });
+});
+
+app.get('/',function(req, res){
     fs.readFile('index.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
         res.write(data);
         res.end();
     });
-}).listen(8000);
+});
+
+app.listen(8000, function(){
+	console.log('start');
+});
+
+
 
