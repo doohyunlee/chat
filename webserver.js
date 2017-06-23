75e8005d35aaac236f8b83781d80cb773809d53a
bbongdoo <bbongdoo@qdillion.com>
2017-06-23 17:26:16 +0900

    12231
    23123

--- webserver.js
+++ webserver.js
@@ -40,9 +40,6 @@ app.get('/xss', function(req, res){
 });
 
 
-
-
-
 app.get('/',function(req, res){
     fs.readFile('index.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
