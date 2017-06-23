a7a5ec1e7b794ec01bbe7d9225e1c1a8610918d8
doohyunlee <bbongdoo@gmail.com>
2017-06-24 01:00:40 +0900

    1111
    11111

--- webserver.js
+++ webserver.js
@@ -2,55 +2,6 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
-app.get('/me', function(req, res){
-    fs.readFile('me.jpg',function (err, data){
-        res.writeHead(200, {'Content-Type': 'image/jpeg'});
-        res.end(data);
-    });
-});
-
-app.get('/me2', function(req, res){
-    fs.readFile('me2.jpg',function (err, data){
-        res.writeHead(200, {'Content-Type': 'image/jpeg'});
-        res.end(data);
-    });
-});
-
-app.get('/me3', function(req, res){
-    fs.readFile('me3.jpg',function (err, data){
-        res.writeHead(200, {'Content-Type': 'image/jpeg'});
-        res.end(data);
-    });
-});
-
-
-app.get('/css', function(req, res){
-    fs.readFile('css/main.css',function (err, data){
-        res.writeHead(200, {'Content-Type': 'text/css'});
-        res.end(data);
-    });
-});
-
-
-app.get('/xss', function(req, res){
-    fs.readFile('js/xssFilter.js',function (err, data){
-        res.writeHead(200, {'Content-Type': 'text/javascript'});
-        res.end(data);
-    });
-});
-
-
-app.get('/socket', function(req, res){
-    fs.readFile('js/socket.io-1.4.0.js',function (err, data){
-        res.writeHead(200, {'Content-Type': 'text/javascript'});
-        res.end(data);
-    });
-});
-
-
-
-
-
 app.get('/',function(req, res){
     fs.readFile('index.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
@@ -58,6 +9,8 @@ app.get('/',function(req, res){
         res.end();
     });
 });
+
+app.use(express.static('public'));
 
 app.listen(8000, function(){
 	console.log('start');
